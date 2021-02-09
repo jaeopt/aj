947d6fdb233865e822bf8c06c1d8f6e11cc13283
Jae Kim <jae.kim@optimizely.com>
2021-02-09 11:34:42 -0800

    add object

---
+++ mylib/src/main/java/com/example/mylib/MyLibUtils.java
@@ -0,0 +1,9 @@
+package com.example.mylib;
+
+import java.sql.SQLSyntaxErrorException;
+
+public class MyLibUtils {
+    static public void say(){
+        System.out.println("HELLO from MyLib");
+    }
+}
