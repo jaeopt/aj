a6333b0c237fe758f0c3e4a523c56d64b210f728
Jae Kim <jae.kim@optimizely.com>
2021-02-09 11:51:32 -0800

    fix app

--- app/src/main/java/com/example/aj/MainActivity.java
+++ app/src/main/java/com/example/aj/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.mylib.MyLibUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyLibUtils.say();
     }
 }
